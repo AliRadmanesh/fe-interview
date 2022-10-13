b2b617fbd3e008cca043cbc5e410dd9812517f1b
Ali Radmanesh <ali.radmanesh1995@gmail.com>
2022-10-13 17:32:23 +0330

    Add viewport addon for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../**/*.story.mdx', '../**/*.story.@(js|jsx|ts|tsx)'],
   addons: [
     'storybook-dark-mode',
+    '@storybook/addon-viewport',
     {
       name: 'storybook-addon-turbo-build',
       options: { optimizationLevel: 2 },
