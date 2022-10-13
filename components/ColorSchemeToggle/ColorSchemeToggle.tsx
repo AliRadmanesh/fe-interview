929461811f3e709b33ff7af03af6fc12619de4f0
Ali Radmanesh <ali.radmanesh1995@gmail.com>
2022-10-13 18:53:48 +0330

    fix: modify theme toggler

--- components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,17 +1,21 @@
-import { ActionIcon, Group, useMantineColorScheme } from '@mantine/core';
+import { ActionIcon, Group, MantineNumberSize, useMantineColorScheme } from '@mantine/core';
 import { IconSun, IconMoonStars } from '@tabler/icons';
 
-export function ColorSchemeToggle() {
+type ColorSchemeToggleProps = {
+  size?: MantineNumberSize;
+};
+
+export function ColorSchemeToggle({ size = 'lg' }: ColorSchemeToggleProps) {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
   return (
-    <Group position="center" mt="xl">
+    <Group position="center">
       <ActionIcon
         onClick={() => toggleColorScheme()}
-        size="xl"
+        size={size}
         sx={(theme) => ({
           backgroundColor:
-            theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+            theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[1],
           color: theme.colorScheme === 'dark' ? theme.colors.yellow[4] : theme.colors.blue[6],
         })}
       >
