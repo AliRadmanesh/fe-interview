0022db848329588357041097bd22e9ce51b4118e
Ali Radmanesh <ali.radmanesh1995@gmail.com>
2022-10-13 18:55:28 +0330

    Remove theme toggler from main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import { Welcome } from '../components/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle';
 
 export default function HomePage() {
   return (
     <>
       <Welcome />
-      <ColorSchemeToggle />
     </>
   );
 }
